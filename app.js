f965204bde49829d7726ef2814d053ed20693f32
Apple <tiffine.koch@gmail.com>
2016-01-22 12:31:24 -0800

    fixed app.js

--- app.js
+++ app.js
@@ -45,19 +45,4 @@ app.post('/tasks', function(req, res) {
 // spin up server
 app.listen(PORT, function() {
   console.log('Express server listening on port', PORT)
-});
-
-
-// app.post('/tasks', function(req, res) {
-//   // var task = JSON.parse(req.body.task);
-//   fs.writeFile('./tasks.json', req.body.task, function(err) {
-//     if(err) return res.status(400).send(err);
-//     // res.send();
-//   });
-//   fs.readFile('./tasks.json', function(err, data) {
-//     if (err) return res.status(400).send(err);
-//     // var arr = JSON.parse(data);
-//     var task = req.body.task;
-//     res.send(data);
-//   });
-// });
+});;
--- public/js/main.js
+++ public/js/main.js
@@ -1,105 +1,51 @@
 $(document).ready(init);
 
-var taskArray = [];
+var tasks = [];
 var newTask;
 
 function init() {
-  populateTasks();
   $('#newEntry').click(addNewTask);
-  $('.dropList').on('click', findCat)
-  // $('#formEntry').submit(newEntry);
-  console.log(this);
-  $('#catDropDown').on('change', findCat);
   $('table').on('click', '#trash', removeEntries);
-  $('table').on('click', '#fav', attachStatus);
-  $('table').on('dblclick', 'th', sortHeaders);
+  console.log(this);
+  // $('table').on('click', '#fav', attachStatus);
 }
 
-function addNewTask() {
+function addNewTask(event) {
 // event.preventDefault();
 
 var name = $('#taskEntry').val();
 var desc = $('#descEntry').val();
-var date = $('#dateEntry').val();
-// var newTask = $('#formEntry').val();
-var newTask = JSON.stringify({name: name, desc: desc, date: date});
+var date = moment($('#dateEntry').val()).format('ll');
+var newTask = ({name: name, desc: desc, date: date, isComplete: false})
   $.post('/tasks', {task: newTask}) //new object
   .success(function(data) {
-    var $li = $('<div>').text(task.name);
-    $('#template').append($li);
+    $('#taskList').append(data);
+    populateTasks();
   })
   .fail(function(err) {
     alert('fix me');
   });
 }
+
 function populateTasks() {
   $.get('/tasks', function(data) {
-    var $task = makeItHappen(data);
-    $('#template').append($tasks);
+    var $tasks = makeItHappen(data);
+    $('#taskList').append($tasks);
   })
 }
 
-function makeItHappen() {
+function makeItHappen(data) {
   return data.map(function(task){
-   var $tr = $('#template').clone();
-   $tr.removeAttr('id');
-   $tr.children('.name').text(task.name);
-   $tr.children('.desc').text(task.desc);
-   $tr.children('.date').text(task.date);
-
-  return $tr;
- });
-}
-
-function removeEntries() {
-  $(this).closest('tr').remove();
-  // var indexJ = $(this).closest('tr').index();
-  // var $checkbox = $('input:checked').closest('tr').remove();
-}
-
-function attachStatus() {
-  var $change = $(this).closest('tr').css("color","red");
-  console.log(this);
-  var taskClicked = $change.index() - 1;
-  $.post('./status', {"status": taskClicked})
-    .succes(function(data) {
-      $change.css("color", "red");
-  })
-}
-
-function findCat(event) {
-  event.preventDefault();
-  var text = $(this).text();
-  $('#catEntry').text(text).append('<span class="caret"></span>');
-  console.log(text);
-}
-
-function addTask() {
-  var contact = $('#formEntry').val()
-  contactList.push(contact);
-
-}
-
-function updateList() {
-var taskList = $('#taskList');
-
-taskList.children().not('#template').remove();
-
-  var $tasks = taskArray.map(function(task, index) {
     var $tr = $('#template').clone();
     $tr.removeAttr('id');
     $tr.children('.name').text(task.name);
     $tr.children('.desc').text(task.desc);
     $tr.children('.date').text(task.date);
-    return $tr
-    });
-  taskList.append($tasks);
+
+    return $tr;
+ });
 }
 
-function sortHeaders(e) {
-  var $target = $(e.target);
-  console.log($target);
-  var $targetClass = $target.attr('class');
-
-  taskArray = _.sortBy(taskArray, $targetClass);
+function removeEntries() {
+ $('input:checked').closest('tr').remove();
 }
